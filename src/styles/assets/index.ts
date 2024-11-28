e50547c169966e1785027a1e5b21965d35c5a7f7
juliana-kim <busker1004@gmail.com>
2024-11-28 18:11:37 +0900

    feat: add image assets

---
+++ src/styles/assets/index.ts
@@ -0,0 +1,6 @@
+export { default as grTokenDefault1 } from './gr_tokenDefault1.svg'
+export { default as grTokenDefault2 } from './gr_tokenDefault2.svg'
+export { default as grTokenDefault3 } from './gr_tokenDefault3.svg'
+export { default as grTokenDefault4 } from './gr_tokenDefault4.svg'
+export { default as grTokenDefault5 } from './gr_tokenDefault5.svg'
+
